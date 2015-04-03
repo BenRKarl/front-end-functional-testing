eb059548e636f3bfecacd063418144554ba0c213
Ben Karl <bkarl@cyrusinnovation.com>
2015-04-03 10:17:50 -0400

    configure jake to run mocha with the jake test command

--- Jakefile.js
+++ Jakefile.js
@@ -2,3 +2,8 @@ desc('default Jake task');
 task('default', function(){
   console.log('Jake has been jaked!');
 });
+
+desc('mocha tests');
+task('test', function(){
+  jake.exec(['mocha'], {printStdout: true});
+});
